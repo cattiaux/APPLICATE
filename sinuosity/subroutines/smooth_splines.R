a14320f3baf107d202f2729606499c4af05645f6
cattiaux <50629311+cattiaux@users.noreply.github.com>
2019-06-04 12:57:59 +0200

    Update smooth_splines.R

--- sinuosity/subroutines/smooth_splines.R
+++ sinuosity/subroutines/smooth_splines.R
@@ -8,7 +8,7 @@
 #------------------------------------------------------------------------------------------
 
 # Script with base routines (netcdf, splines, etc.)
-source("/cnrm/amacs/USERS/cattiaux/APPLICATE/TOOLS/common/R/rbase.R")
+source("../../rbase.R")
 
 #------------------------------------------------------------------------------------------
 # Getting input files:
